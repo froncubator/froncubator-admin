594134ecba07d768e471c36aaa27e16721c7e35c
Occurs <kostyushkokirill@gmail.com>
2018-02-21 17:23:49 +0300

    img upload

--- app/app.js
+++ app/app.js
@@ -38,7 +38,7 @@ app.use('/static', express.static('./frontend'))
 app.post('/admin/api/v1/upload', async (req, res) => {
     const uploader = new Uploader()
     uploader.uploaderFolder = './images'
-    uploader.host = 'http://dev.konkurs-otrivin.ru'
+    uploader.host = 'http://dev.konkurs-otrivin.ru/admin'
     let url = ''
     if (req.body.url != undefined) {
         let type = await helper.fileType(req.body.url)
